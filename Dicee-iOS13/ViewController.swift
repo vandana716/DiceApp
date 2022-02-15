09e2aa0af4bd837e8481b10d04e4123fbf72926d
Vandana Negi <vandana.negi@fareportal.com>
2022-02-15 12:59:44 +0530

    dice app functionality

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [UIImage(named: "DiceOne.png"), UIImage(named: "DiceTwo.png"), UIImage(named: "DiceThree.png"), UIImage(named: "DiceFour.png"), UIImage(named: "DiceFive.png"), UIImage(named: "DiceSix.png")]
+       //can be done like this
+       //diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewOne.image = diceArray.randomElement() as? UIImage
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
     }
-
-
+    
 }
 
